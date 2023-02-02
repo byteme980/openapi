9b734f60553ccb2bd9f1447a676c9f3622d21d12
Kim Strauch <kas04274@gmail.com>
2023-02-02 11:18:07 -0500

    Initial stoplight commit

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4MjA1OQ/spectral.js?branch_slug=main&token=31e9f6ab-edc5-47d2-b845-449d3636e004";
+export default { extends: ruleset };
